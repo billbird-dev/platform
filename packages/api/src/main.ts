b33396d0ba9fd40462be7c56476c15789b106dc8
soulsam480 <soulsam480@hotmail.com>
2021-11-08 22:19:00 +0530

    build(api): use env PORT in prod

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -42,7 +42,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(configService.get('PORT') || 3000);
 
   console.log(`on ${app.getUrl()}`);
 }
